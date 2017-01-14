13ebfb957426af3947e64a588e758dcef4b69927
riplatt <rodney.i.platt@outlook.com>
2017-01-14 16:39:53 +1000

    Fixing Update Command

--- wifi-herbgarden-mqtt.ino
+++ wifi-herbgarden-mqtt.ino
@@ -15,7 +15,7 @@ SerialLogHandler logHandler(LOG_LEVEL_ALL);
 // Define Object
 SHT1x sht1x(dataPin, clockPin);
 PowerShield batteryMonitor;
-byte server[] = {192, 168, 1, 5};         //MQTT Server Address
+byte server[] = {192, 168, 1, 5}; //MQTT Server Address
 MQTT mqttClient(server, 1883, callback);
 // Define Variables
 unsigned short int _qos2messageid = 0;
@@ -25,6 +25,7 @@ float _tempC;
 float _humidity;
 float _cellVoltage;
 float _stateOfCharge;
+bool _updatePending;
 
 // MQTT callback function
 // recieve message
@@ -34,7 +35,13 @@ void callback(char *topic, byte *payload, unsigned int length)
   memcpy(p, payload, length);
   p[length] = NULL;
   String message(p);
+  message.toLowerCase();
   Log.info("Incoming msg:" + message);
+  mqttClient.publish("herbgarden/status", message);
+  if (message.equals("update"))
+  {
+    _updatePending = true;
+  }
   delay(1000);
 }
 
@@ -68,6 +75,26 @@ void setup()
   Log.info("Subnet: %d.%d.%d.%d", WiFi.subnetMask()[0], WiFi.subnetMask()[1], WiFi.subnetMask()[2], WiFi.subnetMask()[3]);
   Log.info("Gateway: %d.%d.%d.%d", WiFi.gatewayIP()[0], WiFi.gatewayIP()[1], WiFi.gatewayIP()[2], WiFi.gatewayIP()[3]);
   Log.info("SSID: %s", WiFi.SSID());
+
+  //Set update flag
+  _updatePending = false;
+
+  _ms = millis();
+  // Connect to MQTT Broker
+  while ((!mqttClient.isConnected()) && (millis() - _ms < 5000))
+  {
+    Log.info("Setup: Not Connected to MQTT Broker...");
+    mqttClient.connect("herbgarden");
+    delay(100);
+  }
+
+  // add qos callback. If don't add qoscallback, ACK message from MQTT server is ignored.
+  mqttClient.addQosCallback(qoscallback);
+
+  if (mqttClient.isConnected())
+  {
+    mqttClient.subscribe("herbgarden/command", MQTT::QOS1);
+  }
 }
 
 void loop()
@@ -76,7 +103,7 @@ void loop()
   // Connect to MQTT Broker
   while ((!mqttClient.isConnected()) && (millis() - _ms < 5000))
   {
-    Log.info("Not Connected to MQTT Broker...");
+    Log.info("Loop Not Connected to MQTT Broker...");
     mqttClient.connect("herbgarden");
     delay(250);
   }
@@ -89,44 +116,27 @@ void loop()
     _humidity = sht1x.readHumidity();
 
     // MQTT Publish
-    mqttClient.loop();
     Log.info("Publishing - Temp:%.2fC, Humidity:%.4f%%, Volats:%.4fV, Charge:%.4f%%", _tempC, _humidity, batteryMonitor.getVCell(), batteryMonitor.getSoC());
     mqttClient.publish("herbgarden/probe/temperature/value", String::format("%.2f", _tempC));
-    mqttClient.publish("herbgarden/probe/temperature/tag", "TI-0001");
-    mqttClient.publish("herbgarden/probe/temperature/eu", "C");
-    mqttClient.publish("herbgarden/probe/temperature/min", "-40.00");
-    mqttClient.publish("herbgarden/probe/temperature/max", "123.80");
-
     mqttClient.publish("herbgarden/probe/humidity/value", String::format("%.4f", _humidity));
-    mqttClient.publish("herbgarden/probe/humidity/tag", "AI-0001");
-    mqttClient.publish("herbgarden/probe/humidity/eu", "%RH");
-    mqttClient.publish("herbgarden/probe/humidity/min", "0.00");
-    mqttClient.publish("herbgarden/probe/humidity/max", "100.00");
-
     mqttClient.publish("herbgarden/battery/voltage/value", String::format("%.4f", batteryMonitor.getVCell()));
-    mqttClient.publish("herbgarden/battery/voltage/tag", "VI-0001");
-    mqttClient.publish("herbgarden/battery/voltage/eu", "V");
-    mqttClient.publish("herbgarden/battery/voltage/min", "0.00");
-    mqttClient.publish("herbgarden/battery/voltage/max", "20.00");
-
     mqttClient.publish("herbgarden/battery/charge/value", String::format("%.4f", batteryMonitor.getSoC()));
-    mqttClient.publish("herbgarden/battery/charge/tag", "XI-0001");
-    mqttClient.publish("herbgarden/battery/charge/eu", "%");
-    mqttClient.publish("herbgarden/battery/charge/min", "0.00");
-    mqttClient.publish("herbgarden/battery/charge/max", "100.00");
-
+    mqttClient.loop();
   }
 
   // Check for a update
   Log.info("Checking for update...");
-  while ((!System.updatesPending()) && (millis() - _ms < 10000))
+  _ms = millis();
+  while ((!_updatePending) && (millis() - _ms < 10000))
   {
+    mqttClient.loop();
     Particle.process();
   }
 
-  if (System.updatesPending())
+  if (_updatePending)
   { // if update is pending hang around for it a minute - will reboot after update
     Log.info("Update Avaliable, Doing update...");
+    mqttClient.publish("herbgarden/status", "waiting");
     _ms = millis();
     while (millis() - _ms < 60000)
     {
